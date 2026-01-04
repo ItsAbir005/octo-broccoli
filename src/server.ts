1d71e754d57cf8435063198da3c30ca9fce62848
Abir Maity <abirmaity44@gmail.com>
2026-01-04 18:45:45 +0530

    global error

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import { WebSocketServer } from "ws";
-
+import {Request,Response,NextFunction} from "express";
 const app = express();
 const server = http.createServer(app);
 
@@ -14,7 +14,18 @@ app.get("/", (_, res) => {
 wss.on("connection", (ws) => {
   ws.send("Connected to WebSocket");
 });
-
+app.get("/error-test", () => {
+  throw new Error("Test server failure");
+});
+app.use(
+  (err: any,req: Request,res: Response,next: NextFunction) => {
+    console.error("Error Log:", err.message);
+    res.status(500).json({
+      success: false,
+      error: "Something went wrong on server",
+    });
+  }
+);
 server.listen(8080, () => {
-  console.log("Server started on port 8080");
+  console.log("Server started on port 8080",{url: "http://localhost:8080"});
 });
