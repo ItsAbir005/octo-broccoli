086d7034b4b9cbd951446392a2b8f282d39c1bd3
Abir Maity <abirmaity44@gmail.com>
2026-01-06 17:48:53 +0530

    add assync warpper

---
+++ src/auth/refreshTokenService.ts
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { AppError } from "../error/AppError";
+
+const REFRESH_SECRET = process.env.JWT_REFRESH!;
+const ACCESS_SECRET = process.env.JWT_ACCESS!;
+
+export const generateAccessToken = (userId: string) => {
+  return jwt.sign({ id: userId }, ACCESS_SECRET, { expiresIn: "15m" });
+};
+
+export const generateRefreshToken = (userId: string) => {
+  return jwt.sign({ id: userId }, REFRESH_SECRET, { expiresIn: "7d" });
+};
+
+export const verifyRefreshToken = (token: string) => {
+  try {
+    return jwt.verify(token, REFRESH_SECRET) as { id: string };
+  } catch {
+    throw new AppError("Invalid or expired refresh token", 401);
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,28 @@
 import express from "express";
 import http from "http";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import { WebSocketServer } from "ws";
 import {Request,Response,NextFunction} from "express";
 import globalErrorHandler from "./error/GlobalError";
 import AppError from "./error/AppError";
 import { requestLogger } from "./middleware/logger.middleware";
+import { asyncWrapper } from "./middleware/asyncWarpper";
+import { generateAccessToken, generateRefreshToken, verifyRefreshToken } from "./auth/refreshTokenService";
 const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(requestLogger);
 const server = http.createServer(app);
-
 const wss = new WebSocketServer({ server, path: "/ws" });
-
-app.get("/", (_, res) => {
-  res.send("Server is running");
-});
-
 wss.on("connection", (ws) => {
   ws.send("Connected to WebSocket");
 });
-app.get("/error-test", (req, res, next) => {
-  try {
-    throw new Error("Test server failure");
-  } catch (error) {
-    next(error);
-  }
-});
-app.use(requestLogger);
-app.use(globalErrorHandler);
+app.get("/error", asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+  throw new AppError("This is a test error", 500);
+}));
 server.listen(8080, () => {
   console.log("Server started on port 8080",{url: "http://localhost:8080"});
 });
+app.use(globalErrorHandler);
